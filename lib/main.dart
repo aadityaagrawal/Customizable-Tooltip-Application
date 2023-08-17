df82b5049aba1fe4d12e4a40b81f2076dfcc0851
aadityaagrawal <adityakuagrawal@gmail.com>
2023-08-18 03:48:15 +0530

    removing unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:trektip/config/app_route.dart';
 import 'package:trektip/config/app_theme.dart';
-// import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'dart:io';
 
 void main() {
-  //Only in testing mode
-  // if (Platform.isWindows || Platform.isLinux) {
-  //   // Initialize FFI
-  //   sqfliteFfiInit();
-  // }
-  // // Change the default factory. On iOS/Android, if not using `sqlite_flutter_lib` you can forget
-  // // this step, it will use the sqlite version available on the system.
-  // databaseFactory = databaseFactoryFfi;
   runApp(const MyApp());
 }
 
